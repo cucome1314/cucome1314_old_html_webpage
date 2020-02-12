8030849ea91dec3dabe794e36ca4fea537a09fa5
cucome1314 <60913870+cucome1314@users.noreply.github.com>
2020-02-12 17:54:20 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+//var head=document.querySelector('h1');
+//head.textContent='hello head';
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+    var head=document.querySelector('h1');
+    head.textContent='hello head';
+}
+
+var myImage = document.querySelector('#a');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'image/1.jpg') {
+      myImage.setAttribute ('src','image/2.jpg');
+      myImage.setAttribute()
+    } else {
+      myImage.setAttribute ('src','image/1.jpg');
+    }
+}
+
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your waifu.');//(input)
+    localStorage.setItem('name', myName);//(tag,vul)
+    myHeading.innerHTML = 'HI  ' + myName;
+}
+/*if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'HI  ' + storedName;
+}*/
+var myButton = document.querySelector('button');
+myButton.onclick=function(){
+    setUserName();
+}
